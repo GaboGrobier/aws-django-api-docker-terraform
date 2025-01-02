66ac4c2b941456b8e570e757514eb8bce5fea831
Gabriel GRobier <gabriel.grobier@gmail.com>
2025-01-02 14:03:48 -0300

    se obtiene ip desde aws cli

--- terraform/build_ec2/modules/ec2/main.tf
+++ terraform/build_ec2/modules/ec2/main.tf
@@ -8,9 +8,20 @@ resource "aws_instance" "desafio_tf_instance" {
     }
      user_data = <<-EOF
                     #!/bin/bash
-                    sudo apt update 
-                    sudo apt upgrade
-                    sudo apt-get install -y docker docker-compose
+                    sudo apt-get update 
+                    sudo apt-get upgrade
+                    sudo apt-get install ca-certificates curl
+                    sudo install -m 0755 -d /etc/apt/keyrings
+                    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+                    sudo chmod a+r /etc/apt/keyrings/docker.asc
+                    echo \
+                      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                    sudo apt-get update
+                    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+                    sudo chmod +x /usr/local/bin/docker-compose
                     sudo usermod -aG docker $USER
                     sudo newgrp docker
                     mkdir /home/ubuntu/web

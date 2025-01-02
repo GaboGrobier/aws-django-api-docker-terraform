901eec7595940dd05eaac25dadcaad3347981192
Gabriel GRobier <gabriel.grobier@gmail.com>
2025-01-02 14:21:37 -0300

    se obtiene ip desde aws cli

--- terraform/build_ec2/modules/ec2/main.tf
+++ terraform/build_ec2/modules/ec2/main.tf
@@ -11,6 +11,8 @@ resource "aws_instance" "desafio_tf_instance" {
                     sudo apt-get update 
                     sudo apt-get upgrade
                     sudo apt install docker.io -y
+                    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+                    chmod +x /usr/local/bin/docker-compose
                     sudo usermod -aG docker $USER
                     sudo newgrp docker
                     mkdir /home/ubuntu/web

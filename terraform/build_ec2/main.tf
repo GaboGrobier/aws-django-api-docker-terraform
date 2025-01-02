f4b6567e725ba10a1c21697c81e7f690c839596b
Gabriel GRobier <gabriel.grobier@gmail.com>
2025-01-02 15:32:18 -0300

    prueba de scan - ecr y ec2

--- terraform/build_ec2/main.tf
+++ terraform/build_ec2/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "bkt-tf-tfstate-gabogrobier-desafiolatam"
-    key = "pruebafinal/desafiolatam/terraform.tfstate"
+    key = "pruebafinal/desafiolatam/ec2/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "db-dynamo-tf-tfstate-gabogrobier-desafiolatam"
     encrypt = true
